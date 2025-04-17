05d5505cf1011a798316b292d93f4a12747b0f29
“henry8913” <tarantola3.0>
2025-04-17 15:37:15 +0200

    0.1.4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = 8913;
+const port = process.env.PORT || 8913;
 
 // Middleware
 app.use(cors());
